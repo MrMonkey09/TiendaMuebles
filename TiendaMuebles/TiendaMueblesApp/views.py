c2e5a50a4e0c0a2a5d43cfdd1724bf923058d7e5
TMonkeyMQ <fabian.m.q@hotmail.com>
2022-12-19 22:15:49 -0300

    Upgrade vista "Envía tus Diseños"

--- TiendaMuebles/TiendaMueblesApp/views.py
+++ TiendaMuebles/TiendaMueblesApp/views.py
@@ -9,16 +9,17 @@ def productos (peticion):
     data = {"titulo": "Productos"}
     return render(peticion, 'productos/productos.html', data)
 
+""" Fabian responsable """
 def solicitudDiseño (peticion):
     data = {"titulo": "¡Envianos tus diseños!"}
     return render(peticion, 'solicitudDiseños/solicitudDiseños.html', data)
+
+""" Fabian responsable - final """
 
 def seguimiento (peticion):
     data = {"titulo": "Seguimiento"}
     return render(peticion, 'seguimiento/seguimiento.html', data)
 
-""" Yeinson responsable """
 def contactanos (peticion):
     data = {"titulo": "Contactanos"}
     return render(peticion, 'contactanos/contactanos.html', data)
-""" Fin Yeinson responsable """
