d98b715c6cc3bc9244b778b7229a751b41eefc69
MrMonkey09 <alfredoa_gestay@outlook.com>
2022-12-20 19:48:10 -0300

    Actualizacion

--- TiendaMuebles/TiendaMueblesApp/models.py
+++ TiendaMuebles/TiendaMueblesApp/models.py
@@ -9,33 +9,41 @@ class producto (models.Model):
     Alto = models.PositiveIntegerField()
     Ancho = models.PositiveIntegerField()
     Largo = models.PositiveIntegerField()
+    Imagenes = models.FileField(upload_to='static/images/productos/')
     
-class imagen (models.Model):
-    id_producto = models.ForeignKey(producto, on_delete=models.CASCADE)
-    Ruta = models.ImageField(upload_to='static/images/')
-
-class Archivo_Complementario (models.Model):
-    id_producto = models.ForeignKey(producto, on_delete=models.CASCADE)
-    Ruta = models.ImageField(upload_to='static/images/')
-    
+class diseño (models.Model):
+    Nombre_Apellido =  models.CharField(max_length=100, null=False)
+    Correo = models.EmailField() 
+    Numero_Contacto = models.IntegerField()
+    Asunto = models.CharField(max_length=256, null=False)
+    Contenido_Mensaje = models.TextField(null=False)
+    Archivos_Complementarios = models.FileField(upload_to='static/docs/')
 
 class venta (models.Model):
-    id_Producto = models.ForeignKey(producto, on_delete=models.CASCADE)
     Cantidad_Productos = models.PositiveIntegerField()
     Total = models.PositiveIntegerField()
     Fecha = models.DateField(auto_now=False, auto_now_add=False)
+    Nombre_usuario = models.CharField(max_length=50)
+    Rut = models.CharField(max_length=15)
+    Email = models.CharField(max_length=50)
+    Telefono = models.CharField(max_length=50)
+    id_Producto = models.ForeignKey(producto, on_delete=models.CASCADE)
     
 class admin (models.Model):
     Correo = models.CharField(max_length=50)
     Contraseña = models.CharField(max_length=20)
     
-class usuario (models.Model):
-    Nombre_usuario = models.CharField(max_length=50)
-    Rut = models.CharField(max_length=15)
-    Email = models.CharField(max_length=50)
-    Telefono = models.CharField(max_length=50)
-    id_venta = models.ForeignKey(venta, on_delete=models.CASCADE)
-    id_Producto = models.ForeignKey(producto, on_delete=models.CASCADE)
-    
 class producto_promocionado (models.Model):
-    id_imagen = models.ForeignKey(imagen, on_delete=models.CASCADE)
+    id_producto = models.ForeignKey(producto, on_delete=models.CASCADE)
+
+class contacto (models.Model):
+    Nombre_Apellido =  models.CharField(max_length=100, null=False)
+    Correo = models.EmailField() 
+    Numero_Contacto = models.IntegerField()
+    Asunto = models.CharField(max_length=256, null=False)
+    Contenido_Mensaje = models.TextField(null=False)
+
+class estado (models.Model):
+    id_producto = models.ForeignKey(producto, on_delete=models.CASCADE, null=True, blank=True)
+    id_diseño = models.ForeignKey(diseño, on_delete=models.CASCADE, null=True, blank=True)
+    nombre_estado = models.CharField(max_length=256, null=False, blank=False)
