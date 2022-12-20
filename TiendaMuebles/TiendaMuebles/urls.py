b4339cd84c1b0e35de9ccbe7af58a73dc85ac3a8
MrMonkey09 <alfredoa_gestay@outlook.com>
2022-12-20 01:25:52 -0300

    Update urls.py

--- TiendaMuebles/TiendaMuebles/urls.py
+++ TiendaMuebles/TiendaMuebles/urls.py
@@ -26,6 +26,3 @@ urlpatterns = [
     path('seguimiento/', views.seguimiento),
     path('contactanos/', views.contactanos)
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
